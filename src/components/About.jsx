321a0a21731f606de5b072fe94bfb70186b6f0cc
Jerricke <eejerrickw@gmail.com>
2023-07-13 16:25:12 -0600

    complete

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,11 +7,10 @@ function About() {
         <div id="About" className="aboutSection">
             <div className="aboutMe fade-in">
                 <h1>My Passion 🔥</h1>
-                <p>Growing up with tech always right beside me, I&apos;ve spent countless amounts of time playing around with different web and mobile applicaitons. 
-                <br/>As I finished by bachelor of science degree at UC San Diego, I grew more and more interested in building applications that would improve our daily lives.
-                <br/>With the desire to build, I&apos;ve picked up programming on my free time, and ultimately decided to attend Flatiron School Software Engineering Bootcamp.
-                <br/>Everyday I work with like-minded peers and hone my skills and craft with the goal of creating the most intuitive and well-rounded applications.
-                </p>
+                <p>Growing up with tech always right beside me, I&apos;ve spent countless amounts of time playing around with different web and mobile applicaitons. </p>
+                <p>As a student at Flatiron school, not only am I learning programming at a face pace environment, I am constantly pushing myself to learn more than the curriculum.</p>
+                <p>With the desire to build, I&apos;ve picked up many different programming skills to deliver the most interactive and engaging applications.</p>
+                <p>Everyday I work with like-minded peers and hone my skills and craft. I firmly belive that there is never an end to learning.</p>
             </div>
             <div className="aboutAnimation fade-in">
                 <VisualAnimation />
